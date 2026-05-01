a6a535944d8335f9a894ddde926e55e53bdac30b
Magnus <magnus.arge@gmail.com>
2026-05-01 07:05:10 +0300

    Testable

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,72 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
-import 'package:estinol_editor_2/main.dart';
+import 'package:estinol_editor_2/providers/dictionary_provider.dart';
+import 'package:estinol_editor_2/widgets/word_editor.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  Future<void> _pumpEditor(WidgetTester tester) async {
+    final provider = DictionaryProvider(initChangesListener: false);
+    provider.startAddingNewWord();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ChangeNotifierProvider<DictionaryProvider>.value(
+            value: provider,
+            child: const WordEditor(),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Ctrl+B wraps selection in ** **', (WidgetTester tester) async {
+    await _pumpEditor(tester);
+
+    final fields = find.byType(TextField);
+    expect(fields, findsNWidgets(3));
+
+    final editorField = fields.at(2);
+    await tester.tap(editorField);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final editable = tester.widget<EditableText>(find.descendant(of: editorField, matching: find.byType(EditableText)));
+    editable.controller.text = 'hello';
+    editable.controller.selection = const TextSelection(baseOffset: 0, extentOffset: 5);
+    await tester.pump();
+
+    await tester.sendKeyDownEvent(LogicalKeyboardKey.controlLeft);
+    await tester.sendKeyEvent(LogicalKeyboardKey.keyB);
+    await tester.sendKeyUpEvent(LogicalKeyboardKey.controlLeft);
+    await tester.pump();
+
+    expect(editable.controller.text, '**hello**');
+  });
+
+  testWidgets('Ctrl+I wraps selection in * *', (WidgetTester tester) async {
+    await _pumpEditor(tester);
+
+    final fields = find.byType(TextField);
+    expect(fields, findsNWidgets(3));
+
+    final editorField = fields.at(2);
+    await tester.tap(editorField);
+    await tester.pump();
+
+    final editable = tester.widget<EditableText>(find.descendant(of: editorField, matching: find.byType(EditableText)));
+    editable.controller.text = 'hello';
+    editable.controller.selection = const TextSelection(baseOffset: 0, extentOffset: 5);
+    await tester.pump();
+
+    await tester.sendKeyDownEvent(LogicalKeyboardKey.controlLeft);
+    await tester.sendKeyEvent(LogicalKeyboardKey.keyI);
+    await tester.sendKeyUpEvent(LogicalKeyboardKey.controlLeft);
+    await tester.pump();
+
+    expect(editable.controller.text, '*hello*');
   });
 }
